69855b87afab0c147afe8b7478377031e4e916f0
cyator <christianyator7@gmail.com>
2023-05-30 08:13:49 +0300

    fix: graphql request connection url

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,6 @@ import { GraphQLClient } from 'graphql-request'
 import { getSdk } from './graphql/generated'
 
 
-const gqlClient = new GraphQLClient("http://localhost:3000/api/graphql");
+const gqlClient = new GraphQLClient("/api/graphql");
 
 export const { getAllProducts } = getSdk(gqlClient)
